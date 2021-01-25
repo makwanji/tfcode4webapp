b064ebc11510a36fa6c1b6ce61ca174994db7d23
makwana.jignesh@gmail.com <makwana.jignesh@gmail.com>
2021-01-26 07:34:11 +0800

    Added route to internet

--- infra/rt.tf
+++ infra/rt.tf
@@ -1,14 +1,14 @@
-# resource "aws_default_route_table" "route_table" {
-#   default_route_table_id = aws_vpc.vpc.default_route_table_id
+resource "aws_default_route_table" "route_table" {
+  default_route_table_id = aws_vpc.rail_vpc.default_route_table_id
 
-# route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.gw.id
-#   }
+route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.rail_igw.id
+  }
 
-# tags = {
-#     Name = "default route table"
-#     env  = var.env
-#   }
+tags = {
+    Name = "Default route table"
+    # env  = var.env
+  }
 
-# }
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,8 @@
 ## variables.tf of module
 variable "aws_region" {
-  default = "ap-south-1"
+  # default = "ap-south-1"
+  default = "us-east-1"
 }
-
 
 variable "cidr_block" {
   default = "10.0.0.0/16"
@@ -19,5 +19,6 @@ variable "SubnetB" {
 }
 
 variable "image_id" {
-  default = "ami-0a4a70bd98c6d6441"
+  # default = "ami-0a4a70bd98c6d6441" 
+  default = "ami-0885b1f6bd170450c"
 }
