13042cfd2d257e1a8e339afe3720d285fb2661d3
Jignesh Makwana <makwana.jignesh@gmail.com>
2021-06-21 09:18:00 +0800

    Updated TF version

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -1,9 +1,4 @@
-provider "aws" {
-  profile = "default"
-  # region  = "us-east-1"
-  region = var.aws_region
-}
-
+# Remote backend
 terraform {
   backend "s3" {
     bucket = "terrafrom-state-staging-jm"
@@ -12,3 +7,18 @@ terraform {
   }
 }
 
+# Required provider 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# AWS provider
+provider "aws" {
+  profile = "default"
+  region = var.aws_region
+}
